2dbb0a7d7b34a8d0da099c7e1b53f6efc3f363c6
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-11-20 17:50:46 +0900

    Create 과일로_만든_아이스크림_고르기.sql

---
+++ lv1/과일로_만든_아이스크림_고르기.sql
@@ -0,0 +1,6 @@
+SELECT f.flavor flavor
+from first_half f 
+join icecream_info i 
+on f.flavor = i.flavor
+where f.total_order > 3000 and i.ingredient_type like 'fruit_based' 
+order by f.total_order desc;
