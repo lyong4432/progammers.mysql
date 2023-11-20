9887b5cbc7dcf9e8d421be043f70ec5b942cb5df
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-11-20 17:49:04 +0900

    Create 조건에_맞는_도서와_저자_리스트_출력하기.sql

---
+++ lv2/조건에_맞는_도서와_저자_리스트_출력하기.sql
@@ -0,0 +1,6 @@
+SELECT b.book_id, a.author_name, date_format(b.published_date,"%Y-%m-%d") published_date
+from book b
+join author a
+on b.author_id = a.author_id
+where b.category like '경제'
+order by published_date;
