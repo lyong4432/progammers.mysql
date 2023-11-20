38f4bb405ae5d242588775dfe6a63ab90efb645e
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-11-20 17:46:43 +0900

    Create 자동차_대여_기록에서_장기단기_대여_구분하기.sql

---
+++ lv1/자동차_대여_기록에서_장기단기_대여_구분하기.sql
@@ -0,0 +1,8 @@
+SELECT history_id, car_id, 
+date_format(start_date,'%Y-%m-%d') as start_date, 
+date_format(end_date,'%Y-%m-%d') as end_date, 
+if(datediff(end_date,start_date)>=29, '장기 대여','단기 대여')
+as rent_type
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+Where start_date between '2022-09-01' and '2022-09-30'
+order by history_id desc;
