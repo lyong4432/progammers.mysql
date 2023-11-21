edfb9c8da22841e38484d78ee6673bbcaac89aef
Hwajung Choi <80114703+lyong4432@users.noreply.github.com>
2023-11-21 11:34:27 +0900

    Create 취소되지_않은_진료_예약_조회하기.sql

---
+++ lv4/취소되지_않은_진료_예약_조회하기.sql
@@ -0,0 +1,11 @@
+SELECT a.apnt_no, p.pt_name, p.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
+from appointment a 
+    join patient p 
+    on a.pt_no = p.pt_no 
+    join doctor d
+    on a.mddr_id = d.dr_id
+
+where year(a.apnt_ymd) = 2022 and month(a.apnt_ymd) =4 and day(a.apnt_ymd) = 13
+and a.apnt_cncl_yn like "N" 
+
+order by 6;
